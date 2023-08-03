63caf1a365ea87a77e5c44b9af7f0b82c2884c2d
Mawadah Elmashad <mawadahelmashad@std.mans.edu.eg>
2023-08-03 19:17:00 +0300

    commited

---
+++ data.js
@@ -0,0 +1,27 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'musics/yt1s.com - Maher Zain  Assalamu Alaika Arabic  Vocals Only  ماهر زين  السلام عليك   بدون موسيقى.mp3',
+        artist: 'artist 1',
+        cover: 'images/flowers.webp'
+    },
+    {
+        name: 'song 2',
+        path: 'musics/yt1s.com - Maher Zain  Hubb Ennabi Loving the Prophet  Vocals Only ماهر زين  حب النبي  بدون موسيقى.mp3',
+        artist: 'artist 2',
+        cover: 'images/mountains.jpeg'
+    },
+    {
+        name: 'song 3',
+        path: 'musics/yt1s.com - Maher Zain  Insha Allah  ماهر زين  إن شاء الله بدون موسيقى  Vocals Only Lyrics.mp3',
+        artist: 'artist 3',
+        cover: 'images/sea.jpeg'
+    },
+    {
+        name: 'song 4',
+        path: 'musics/yt1s.com - Maher Zain  Muhammad Pbuh  Vocals Only Lyrics.mp3',
+        artist: 'artist 4',
+        cover: 'images/sky.jpeg'
+    }
+
+];
